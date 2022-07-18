c525a084ef6e52ff4fed6591fae476a4a5978e67
heannnn <jse033101@gmail.com>
2022-07-18 20:49:26 +0900

    Room 안의 인원 세기

--- src/server.js
+++ src/server.js
@@ -30,6 +30,10 @@ function publicRooms() {
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName).size;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anno";
   socket.onAny((event) => {
@@ -38,19 +42,19 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname);
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname)
+    socket.rooms.forEach((roomName) =>
+      socket.to(roomName).emit("bye", socket.nickname, countRoom(roomName) - 1)
     );
   });
   socket.on("disconnect", () => {
     wsServer.sockets.emit("room_change", publicRooms());
   });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
+  socket.on("new_message", (msg, roomName, done) => {
+    socket.to(roomName).emit("new_message", `${socket.nickname} : ${msg}`);
     done();
   });
   socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
